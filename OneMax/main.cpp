fa77e93312630b315a954b116b9150693adeed7c
Oscar Chen <oscar11902@gmail.com>
2019-09-24 17:57:05 +0800

    first init

---
+++ OneMax/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+    vector <bool> bitstring;
+    int runs,iterators;
+    double avgresult = 0.0;
+
+    for(int i = 1 ; i < argc ; i++){
+        char *arg = argv[i];
+        switch (arg[0])
+        {
+        case '-':
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+    
+    
+    return 0;
+}
