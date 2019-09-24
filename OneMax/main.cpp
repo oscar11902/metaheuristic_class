cdc42d0434666be7befaa07802665f04cd0ddd05
oscar11902 <oscar11902@gmail.com>
2019-09-24 21:20:01 +0800

    init command

--- OneMax/main.cpp
+++ OneMax/main.cpp
@@ -3,25 +3,39 @@
 
 using namespace std;
 
-int main (int argc, char *argv[]){
-    vector <bool> bitstring;
-    int runs,iterators;
+int main(int argc, char *argv[])
+{
+    vector<bool> bitstring;
+    int runs, iterators, length;
     double avgresult = 0.0;
 
-    for(int i = 1 ; i < argc ; i++){
+    for (int i = 1; i < argc; i++)
+    {
         char *arg = argv[i];
         switch (arg[0])
         {
         case '-':
-            /* code */
+            switch (arg[1])
+            {
+            case 'l':
+                i++;
+                length = atoi(argv[i]);
+                break;
+            case 'i':
+                i++;
+                iterators = atoi(argv[i]);
+                break;
+            case 'r':
+                i++;
+                runs = atoi(argv[i]);
+                break;
+            
+            }
             break;
-        
         default:
             break;
         }
     }
-    
-    
-    
+
     return 0;
 }
