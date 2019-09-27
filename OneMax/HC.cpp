762ebbdab995f61131dc93ee9f6ce5985a141439
oscar11902 <oscar11902@gmail.com>
2019-09-27 13:51:53 +0800

    reslove change problem

--- OneMax/HC.cpp
+++ OneMax/HC.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void HC::initstring(vector<bool> &bitstring)
 {
-    for (bool bit : bitstring)
+    for (int i = 0 ; i < bitstring.size() ; i++)
     {
-        bit = (rand() & 1) ? true : false;
+        bitstring[i] = (rand() & 1) ? true : false;
     }
 }
 void HC::bitchange(vector<bool> &bitstring)
@@ -15,20 +15,36 @@ void HC::bitchange(vector<bool> &bitstring)
     bitstring[index] = !bitstring[index];
 }
 
+void HC::clonestring(vector<bool> &newstring, vector<bool> orignstring)
+{
+    newstring.clear();
+    newstring.assign(orignstring.begin(), orignstring.end());
+}
+
 double HC::exe(vector<bool> bitstring, int runs, int iterators)
 {
     double result = 0.0;
     int round = runs;
     while (round--)
     {
+        //Initialize
         initstring(bitstring);
         int time = iterators;
         int value = evaluate(bitstring);
         while (time--)
         {
-            bitchange(bitstring);
-            int newvalue = evaluate(bitstring);
-            value = newvalue > value ? newvalue : value;
+            //Transfer
+            vector<bool> tmpstring;
+            clonestring(tmpstring, bitstring);
+            bitchange(tmpstring);
+            //Evalueate
+            int newvalue = evaluate(tmpstring);
+            //Determine
+            if (newvalue > value)
+            {
+                value = newvalue;
+                clonestring(bitstring, tmpstring);
+            }
         }
         result += value;
     }
--- OneMax/HC.h
+++ OneMax/HC.h
@@ -9,6 +9,7 @@ class HC
 private:
     void initstring(std::vector<bool> &);
     void bitchange(std::vector<bool> &);
+    void clonestring(std::vector <bool>& , std::vector<bool>);
 
 public:
     double exe(std::vector<bool> , int ,int);
